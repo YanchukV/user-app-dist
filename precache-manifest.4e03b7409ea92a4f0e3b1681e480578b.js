e1c3add0812121a6ab0de604f44a85800399d483
Vyacheslav <jantschuk@gmail.com>
2020-10-05 00:37:47 +0300

    first commit

---
+++ precache-manifest.4e03b7409ea92a4f0e3b1681e480578b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac9c53ac6d2adcb225a431b04d461f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc1c6c3cbe262f134dce",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b9539c402b7ad44e06b8",
+    "url": "./static/css/main.e92e0b94.chunk.css"
+  },
+  {
+    "revision": "dc1c6c3cbe262f134dce",
+    "url": "./static/js/2.2b22f4cd.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "./static/js/2.2b22f4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9539c402b7ad44e06b8",
+    "url": "./static/js/main.7f421e11.chunk.js"
+  },
+  {
+    "revision": "e0ae8a993b2a970f9d5b",
+    "url": "./static/js/runtime-main.b372d3df.js"
+  }
+]);
